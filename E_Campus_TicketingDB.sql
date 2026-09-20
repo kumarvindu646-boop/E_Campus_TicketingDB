98258cca7760f6e344a8491b4f53531813fa8c80
Vindu Kumar <kumarvindu646@gmail.com>
2026-09-20 10:05:16 +0530

    Update E_Campus_TicketingDB.sql

--- E_Campus_TicketingDB.sql
+++ E_Campus_TicketingDB.sql
@@ -13,3 +13,26 @@ CREATE TABLE users (
     created_at      TIMESTAMP     DEFAULT CURRENT_TIMESTAMP,
     updated_at      TIMESTAMP     DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
+
+--Department fields 
+
+CREATE TABLE departments (
+    department_id   INT AUTO_INCREMENT PRIMARY KEY,
+    dept_name        VARCHAR(100) NOT NULL,
+    dept_code        VARCHAR(20)  UNIQUE
+);
+--For Student Details 
+CREATE TABLE students (
+    student_enrol_id      BIGINT PRIMARY KEY,                
+    roll_no         VARCHAR(30)  NOT NULL UNIQUE,
+    semester        TINYINT,
+    batch_year      YEAR,
+    is_hosteller    BOOLEAN DEFAULT FALSE,
+    hostel_reg_id       INT NULL,
+    room_num         INT NULL,
+    student_phone  VARCHAR(15),
+    guardian_name   VARCHAR(150),
+    guardian_phone  VARCHAR(15),
+    FOREIGN KEY (student_id)    REFERENCES users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (department_id) REFERENCES departments(department_id)
+);
